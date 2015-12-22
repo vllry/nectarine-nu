647ae630fd9829ea235605f9341f11a068adfe0e
Vallery Lancey <vallerylancey@gmail.com>
2015-12-21 22:42:18 -0800

    Cross platform support*
    *Actually just pushing between workstations

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
 	res.json({'success':true});
 });
 
 router.post('/settings/mode', function(req, res) {
+	console.log(req.body);
+	res.json({'success':true});
+});
+
+router.post('/settings/temp', function(req, res) {
 	console.log(req.body);
 	res.json({'success':true});
 });
