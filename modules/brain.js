385333dda626bcfa56f6ab321f0045e18e0a6154
Vallery Lancey <vallerylancey@gmail.com>
2015-12-23 16:04:01 -0800

    brain.js now turns the switch on or off based on temp

--- modules/brain.js
+++ modules/brain.js
@@ -1,5 +1,6 @@
 var database = require('../database');
 var temperature = require('./temperature');
+var powertail = require('./powertail');
 
 
 
@@ -8,12 +9,11 @@ exports.run = function() {
 		temperature.getTemperature(function (current) {
 			database.settingGet('temp', function(desired) {
 				var desired = Number(desired.temp);
-				console.log(desired);
 				if (current - desired > 1) {
-					console.log('off');
+					powertail.set(false);
 				}
 				else if (current - desired < -1) {
-					console.log('on')
+					powertail.set(true);
 				}
 				else {
 					console.log(current - desired);
@@ -24,5 +24,5 @@ exports.run = function() {
 		}, function (err) {
 			console.log(err);
 		});
-	}, 10 * 1000);	//Every 10s
+	}, 20 * 1000);	//Every 20s
 };
--- modules/powertail.js
+++ modules/powertail.js
@@ -2,14 +2,15 @@ var gpio = require("pi-gpio"); //https://www.npmjs.com/package/pi-gpio
 
 var config = require("../config");
 
-var pin = config.pins.powertail;
+var pin = config.pins.powertail; //https://ms-iot.github.io/content/images/PinMappings/RP2_Pinout.png
+				//Pin number is based on physical board, not GPIO label
 
 
 
-var set = function(power) {
+exports.set = function(power) {
 	gpio.open(pin, "output", function(err) { //Open pin for output
-		gpio.write(pin, power, function() {
-			gpio.close(pin);
+		gpio.write(pin, power, function() { //"gpio-admin: could not flush data to /sys/class/gpio/export: Device or resource busy" isn't a meaningful error
+			return;
 		});
 	});
-};Edge and point drawing studies 
+};
