af15356a0bf4a756a46fc2c3dec8b0bcab3732de
Ben <nebual@nebtown.info>
2015-12-27 16:33:42 -0800

    Only gpio.open once; close before opening (fixes 'device busy' warning spam)

--- modules/powertail.js
+++ modules/powertail.js
@@ -5,11 +5,24 @@ var config = require("../config");
 var pin = config.pins.powertail; //https://ms-iot.github.io/content/images/PinMappings/RP2_Pinout.png
 				//Pin number is based on physical board, not GPIO label
 
+var connectedPins = {};
 
+var getOutputPin = function (pin, func) {
+	if (connectedPins[pin] !== undefined) {
+		func();
+	} else {
+		gpio.close(pin, function() { // Close it incase it was opened by a previous instance of the app
+			gpio.open(pin, "output", function (err) { //Open pin for output
+				connectedPins[pin] = true;
+				func();
+			});
+		});
+	}
+};
 
-exports.set = function(power) {
-	gpio.open(pin, "output", function(err) { //Open pin for output
-		gpio.write(pin, !power, function() { //"gpio-admin: could not flush data to /sys/class/gpio/export: Device or resource busy" isn't a meaningful error
+exports.set = function (power) {
+	getOutputPin(pin, function () {
+		gpio.write(pin, !power, function () {
 			return;
 		});
 	});
